6c7b5c9e099db7dfa9944d226f9f5ea5dd296cb2
ACACIANha <gkehdrn95@netand.co.kr>
2023-06-20 14:23:32 +0000

    first commit

---
+++ src/main/kotlin/com/example/springauthserverpractice2kt/SpringAuthServerPractice2KtApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springauthserverpractice2kt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAuthServerPractice2KtApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringAuthServerPractice2KtApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springauthserverpractice2kt/SpringAuthServerPractice2KtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springauthserverpractice2kt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringAuthServerPractice2KtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
